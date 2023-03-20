825c298e419528a183c9af43c2a638a4b093382e
Alexander Savushkin <74372739+avsavushkiny@users.noreply.github.com>
2023-03-20 14:27:21 +0300

    upd 200323

--- live/a.cpp
+++ live/a.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
 
+int summa(int a, int b)
+{
+    if (a == 0) throw "error"; //создаем условие для выброса 
+                               //throw бросил текст в catch
+    else return a + b;
+}
+
 int main()
 {
-
+    try
+    {
+        summa(0, 3); //выполняем код
+    }
+    catch(const char* e) //catch ловит то что бросил throw
+    {
+        std::cerr << e << '\n';
+    }
 }
